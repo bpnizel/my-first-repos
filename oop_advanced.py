3e53edfa6146430ebc652068ac52ad284d4df2dd
bpnizel <155309205+bpnizel@users.noreply.github.com>
2025-08-17 10:43:58 +0300

    Update oop_advanced.py
    added from programmer 2

--- oop_advanced.py
+++ oop_advanced.py
@@ -7,7 +7,8 @@ from Employee import Employee
 person = ClassPerson("Alex", 30)
 
 student = Student("mike", 27, "engineer", 3, 70)
-          
+print(this is form progarmmer 2)
+
 
 employee = Employee("john", 40 , "Software Engineer", 4500)
 
@@ -18,4 +19,5 @@ print(type(employee))
 people = [ person, student, employee]
 for person in people:
     print(person.printMySelf())  
+
      
