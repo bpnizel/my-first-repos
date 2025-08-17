30ebc1c5ea923dbb735695c0047a8b9ae0d40586
bpnizel <pnizel@gmail.com>
2025-08-17 12:01:20 +0300

    Rename oop_advanced.py to main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from Class_person import ClassPerson
+from Student import Student
+from Employee import Employee
+ 
+ 
+
+person = ClassPerson("Alex", 30)
+
+student = Student("mike", 27, "engineer", 3, 70)
+
+
+employee = Employee("john", 40 , "Software Engineer", 4500)
+
+
+print(type(employee))
+
+
+people = [ person, student, employee]
+for person in people:
+    print(person.printMySelf())  
+
+     
